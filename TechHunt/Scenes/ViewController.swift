7351aa1ceadb1209e02e4b746bab1fbca6368d94
Anna Sumire <anachikhladze99@gmail.com>
2024-01-18 22:49:17 +0400

    feat: add and organize project folder structure

---
+++ TechHunt/Scenes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechHunt
+//
+//  Created by Anna Sumire on 18.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
