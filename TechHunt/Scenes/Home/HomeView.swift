3d756ff71b9cbb61f4a496019f72f5d868f89bc2
Anna Sumire <anachikhladze99@gmail.com>
2024-01-24 00:34:30 +0400

    feat: add JobCardView

--- TechHunt/Scenes/Home/HomeView.swift
+++ TechHunt/Scenes/Home/HomeView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct HomeView: View {
     
     var body: some View {
-        VStack(spacing: 20) {
+        VStack(spacing: 16) {
             headerView
             discoverJobHStack
             howToUseHStack
+            VCard()
+            trendingHStack
             Spacer()
         }
     }
@@ -48,9 +50,19 @@ struct HomeView: View {
     
     var howToUseHStack: some View {
         HStack {
-            Text("How to use TechHunt?")
+            Text("How to use TechHunt? 🚀")
                 .font(.system(size: 20, weight: .black, design: .rounded))
-                .padding()
+                .padding(.horizontal)
+            
+            Spacer()
+        }
+    }
+    
+    var trendingHStack: some View {
+        HStack {
+            Text("Trending Job 🔥")
+                .font(.system(size: 20, weight: .black, design: .rounded))
+                .padding(.horizontal)
             
             Spacer()
         }
@@ -60,3 +72,51 @@ struct HomeView: View {
 #Preview {
     HomeView()
 }
+
+
+struct VCard: View {
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(LinearGradient(gradient: Gradient(colors: [Color.purple, Color.buttonBackground]), startPoint: .topTrailing, endPoint: .bottomTrailing))
+                .opacity(0.8)
+            
+            VStack {
+                Text("Welcome to TechHunt! Explore top tech jobs, keep up with trends, and craft your CV for free. Apply with a tap, let companies discover you, and refer friends for our New Year’s giveaway. Don’t forget to save your favorite jobs. Enjoy your job hunt!")
+                    .font(.system(size: 18, weight: .light, design: .rounded))
+                    .padding()
+                
+                HStack {
+                    Button(action: {
+                        print("Button tapped!")
+                    }) {
+                        Text("Read More")
+                            .font(.system(size: 16, weight: .black, design: .rounded))
+                            .padding()
+                            .background(Color.buttonBackground)
+                            .foregroundColor(.white)
+                            .cornerRadius(8)
+                    }
+                    
+                    Button(action: {
+                        print("Button tapped!")
+                    }) {
+                        Text("Create CV")
+                            .font(.system(size: 16, weight: .black, design: .rounded))
+                            .padding()
+                            .background(Color.buttonBackground)
+                            .foregroundColor(.white)
+                            .cornerRadius(8)
+                    }
+                    
+                }
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: 250)
+        .cornerRadius(26)
+        .padding(.horizontal)
+    }
+}
+
+
