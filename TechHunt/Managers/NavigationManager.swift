93000b753890cdd74269428497512731b583ef5e
Anna Sumire <anachikhladze99@gmail.com>
2024-02-02 17:30:59 +0400

    fix: Navigation issue

--- TechHunt/Managers/NavigationManager.swift
+++ TechHunt/Managers/NavigationManager.swift
@@ -10,31 +10,45 @@ import UIKit
 
 @available(iOS 17.0, *)
 final class NavigationManager: ObservableObject {
-    private let window: UIWindow
+    private var window: UIWindow?
     
-    init(window: UIWindow) {
+    static let shared = NavigationManager()
+    
+    private init() {}
+    
+    func setWindow(_ window: UIWindow) {
         self.window = window
     }
     
     func showRootView() {
-        let rootView = ProfileView(coordinator: self)
+        let rootView = ProfileView()
         let rootViewHosting = UIHostingController(rootView: rootView)
-        window.rootViewController = UINavigationController(rootViewController: rootViewHosting)
+        window?.rootViewController = UINavigationController(rootViewController: rootViewHosting)
     }
-
+    
     
     func showExperienceVC() {
         let view = ExperienceViewController()
-        if let navigationController = window.rootViewController as? UINavigationController {
+        if let navigationController = window?.rootViewController as? UINavigationController {
             navigationController.pushViewController(view, animated: true)
         }
     }
     
     
     func goBack() {
-        if let navigationController = window.rootViewController as? UINavigationController {
+        if let navigationController = window?.rootViewController as? UINavigationController {
             navigationController.popViewController(animated: true)
         }
     }
     
+    func navigateToExperienceVC() {
+        let experienceViewController = ExperienceViewController()
+        
+        if let tabBarController = window?.rootViewController as? UITabBarController,
+           let navigationController = tabBarController.selectedViewController as?  UINavigationController {
+            navigationController.pushViewController(experienceViewController, animated: true)
+        } else {
+            print("not found")
+        }
+    }
 }
