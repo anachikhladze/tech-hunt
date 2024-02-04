93ef5c7e980ee05282b1f79d7d432725f056e56a
Anna Sumire <anachikhladze99@gmail.com>
2024-02-04 19:16:30 +0400

    feat: add registation view and reusable components

---
+++ TechHunt/Components/PasswordConditionView.swift
@@ -0,0 +1,29 @@
+//
+//  PasswordConditionView.swift
+//  TechHunt
+//
+//  Created by Anna Sumire on 04.02.24.
+//
+
+import SwiftUI
+
+struct PasswordConditionView: View {
+    
+    // MARK: - Properties
+    let condition: Bool
+    let text: String
+    
+    // MARK: - Body
+    var body: some View {
+        HStack {
+            Image(systemName: condition ? "checkmark.circle.fill" : "x.circle")
+                .foregroundStyle(Color("ButtonColor"))
+            Text(text)
+        }
+    }
+}
+
+// MARK: - Preview
+#Preview {
+    PasswordConditionView(condition: false, text: "Test")
+}
