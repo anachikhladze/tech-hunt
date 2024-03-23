deae4f18b9fe5a96d3850fbb0512a4df79f72ed6
Anna Sumire <anachikhladze99@gmail.com>
2024-03-23 19:38:13 +0400

    add settings view controller and custom settings view component

---
+++ TechHunt/Scenes/Settings/CustomSettingsView.swift
@@ -0,0 +1,65 @@
+//
+//  CustomSettingsView.swift
+//  TechHunt
+//
+//  Created by Anna Sumire on 23.03.24.
+//
+
+import UIKit
+
+final class CustomSettingsView: UIView {
+    
+    var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Title"
+        label.font = .customRoundedFont(size: 18, weight: .bold)
+        return label
+    }()
+    
+    var nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .customRoundedFont(size: 18, weight: .light)
+        return label
+    }()
+    
+    let lineView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .systemGray
+        view.alpha = 0.3
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        let stackView = UIStackView(arrangedSubviews: [titleLabel, nameLabel])
+        stackView.axis = .vertical
+        stackView.spacing = 16
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(stackView)
+        
+        addSubview(lineView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor, constant: 8),
+            stackView.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor, constant: -8),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            lineView.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor, constant: 8),
+            lineView.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor, constant: -8),
+            lineView.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 24),
+            lineView.heightAnchor.constraint(equalToConstant: 1)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(with title: String, with name: String) {
+        self.titleLabel.text = title
+        self.nameLabel.text = name
+    }
+}
---
+++ TechHunt/Scenes/Settings/SettingsViewController.swift
@@ -0,0 +1,75 @@
+//
+//  SettingsViewController.swift
+//  TechHunt
+//
+//  Created by Anna Sumire on 23.03.24.
+//
+
+import UIKit
+
+final class SettingsViewController: UIViewController {
+    
+    let emailCustomView: CustomSettingsView = {
+        let emailCustomView = CustomSettingsView()
+        emailCustomView.translatesAutoresizingMaskIntoConstraints = false
+        emailCustomView.configure(with: "Email", with: "test@gmail.com")
+        return emailCustomView
+    }()
+    
+    let fullNameCustomView: CustomSettingsView = {
+        let emailCustomView = CustomSettingsView()
+        emailCustomView.translatesAutoresizingMaskIntoConstraints = false
+        emailCustomView.configure(with: "Full Name", with: "Ana Chikhladze")
+        return emailCustomView
+    }()
+    
+    let mobileNumberCustomView: CustomSettingsView = {
+        let emailCustomView = CustomSettingsView()
+        emailCustomView.translatesAutoresizingMaskIntoConstraints = false
+        emailCustomView.configure(with: "Mobile Number", with: "+995557773047")
+        return emailCustomView
+    }()
+    
+    let passwordCustomView: CustomSettingsView = {
+        let emailCustomView = CustomSettingsView()
+        emailCustomView.translatesAutoresizingMaskIntoConstraints = false
+        emailCustomView.configure(with: "Password", with: "********")
+        return emailCustomView
+    }()
+    
+    let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        setConstraints()
+        
+        navigationItem.title = "Settings"
+        navigationController?.navigationBar.prefersLargeTitles = true
+            
+    }
+    
+    private func setConstraints() {
+        view.addSubview(stackView)
+        stackView.addArrangedSubview(emailCustomView)
+        stackView.addArrangedSubview(fullNameCustomView)
+        stackView.addArrangedSubview(mobileNumberCustomView)
+        stackView.addArrangedSubview(passwordCustomView)
+        
+        stackView.spacing = 30
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            stackView.bottomAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
+        ])
+    }
+
+}
