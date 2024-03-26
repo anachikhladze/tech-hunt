5bcb84d0ad517ceda7ff6b2ba2fdda9978989f6f
Anna Sumire <anachikhladze99@gmail.com>
2024-03-26 13:21:59 +0400

    refactor: refactor settings and update info

--- TechHunt/App/SceneDelegate.swift
+++ TechHunt/App/SceneDelegate.swift
@@ -18,10 +18,9 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = scene as? UIWindowScene else { return }
         window = UIWindow(windowScene: windowScene)
-        navigationManager.navigateToInitialScreen()
+//        navigationManager.navigateToInitialScreen()
         
-//        window?.rootViewController = UINavigationController(rootViewController: UpdatePasswordViewController() )
-        
+        window?.rootViewController = UINavigationController(rootViewController: OnboardingViewController(navigationManager: navigationManager) )
         
         window?.makeKeyAndVisible()
     }
